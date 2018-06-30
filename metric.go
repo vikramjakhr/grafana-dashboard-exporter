9ed26efb2cb5d5fac46421b35143c952f6ceceb9
Vikram Jakhar <vikram.jakhar@tothenew.com>
2018-06-30 14:42:57 +0530

    metric implementation added

---
+++ metric.go
@@ -0,0 +1,17 @@
+package gde
+
+// ValueType is an enumeration of metric types that represent a simple value.
+type ValueType string
+
+// Possible values for the ValueType enum.
+const (
+	Datasource ValueType = "Datasource"
+	Dashboard  ValueType = "Dashboard"
+)
+
+type Metric interface {
+	// Getting data structure functions
+	Name() string
+	Type() ValueType
+	Content() string
+}
---
+++ metric/metric.go
@@ -0,0 +1,27 @@
+package metric
+
+import (
+	"github.com/vikramjakhr/grafana-dashboard-exporter"
+)
+
+type metric struct {
+	name    string
+	mType   gde.ValueType
+	content []byte
+}
+
+func (m *metric) Name() string {
+	return m.name
+}
+
+func (m *metric) Type() gde.ValueType {
+	return m.mType
+}
+
+func (m *metric) Content() string {
+	return string(m.content)
+}
+
+func New(name string, mType gde.ValueType, content []byte) (metric) {
+	return metric{name: name, mType: mType, content: content}
+}
