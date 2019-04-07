b7c24c73c9c7ec28b46ea4d59bd9b6dff333d0bd
Vikram Jakhar <vikram.jakhar@tothenew.com>
2019-04-07 21:13:53 +0530

    docker support added and script changes

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 
+# chkconfig: 2345 99 01
+# description: GDE daemon
+
 ### BEGIN INIT INFO
 # Provides:          gde
 # Required-Start:    $all
@@ -9,7 +12,6 @@
 # Short-Description: Start gde at boot time
 ### END INIT INFO
 
-# If you modify this, please make sure to also edit gde.service
 # this init script supports three different variations:
 #  1. New lsb that define start-stop-daemon
 #  2. Old lsb that don't have start-stop-daemon but define, log, pidofproc and killproc
@@ -18,73 +20,57 @@
 # In the third case we have to define our own functions which are very dumb
 # and expect the args to be positioned correctly.
 
-DEFAULT=/etc/default/gde
+# Command-line options that can be set in /etc/default/gde.  These will override
+# any config file values.
+GDE_OPTS=
 
-# Process name ( For display )
-NAME=gde
-
-# User and group
 USER=gde
 GROUP=gde
-
-# Daemon name, where is the actual executable
-# If the daemon is not there, then exit.
-DAEMON=/usr/bin/gde
-[ -x $DAEMON ] || exit 5
-
-# Configuration file
-CONFIG=/etc/gde/gde.conf
-
-# PID file for the daemon
-PIDFILE=/var/run/gde/gde.pid
-PIDDIR=`dirname $PIDFILE`
-if [ ! -d "$PIDDIR" ]; then
-    mkdir -p $PIDDIR
-    chown $USER:$GROUP $PIDDIR
-fi
-
-# Max open files
-OPEN_FILE_LIMIT=65536
 
 if [ -r /lib/lsb/init-functions ]; then
     source /lib/lsb/init-functions
 fi
 
-# Logging
+DEFAULT=/etc/default/gde
+
+if [ -r $DEFAULT ]; then
+    set -o allexport
+    source $DEFAULT
+    set +o allexport
+fi
+
 if [ -z "$STDOUT" ]; then
     STDOUT=/dev/null
 fi
-
 if [ ! -f "$STDOUT" ]; then
-    mkdir -p $(dirname $STDOUT)
+    mkdir -p `dirname $STDOUT`
 fi
 
 if [ -z "$STDERR" ]; then
-    STDERR=/var/log/gde/gde.err
+    STDERR=/var/log/gde/gde.log
 fi
-
 if [ ! -f "$STDERR" ]; then
-    mkdir -p $(dirname $STDERR)
+    mkdir -p `dirname $STDERR`
 fi
 
-# Overwrite init script variables with /etc/default/gde values
-if [ -r $DEFAULT ]; then
-    source $DEFAULT
-fi
+OPEN_FILE_LIMIT=65536
 
 function pidofproc() {
     if [ $# -ne 3 ]; then
         echo "Expected three arguments, e.g. $0 -p pidfile daemon-name"
     fi
 
-    PID=`pgrep -f $3`
-    local PIDFILE=`cat $2`
-
-    if [ "x$PIDFILE" == "x" ]; then
+    if [ ! -f "$2" ]; then
         return 1
     fi
 
-    if [ "x$PID" != "x" -a "$PIDFILE" == "$PID" ]; then
+    local pidfile=`cat $2`
+
+    if [ "x$pidfile" == "x" ]; then
+        return 1
+    fi
+
+    if ps --pid "$pidfile" | grep -q $(basename $3); then
         return 0
     fi
 
@@ -96,22 +82,9 @@ function killproc() {
         echo "Expected three arguments, e.g. $0 -p pidfile signal"
     fi
 
-    PID=`cat $2`
+    pid=`cat $2`
 
-    /bin/kill -s $3 $PID
-    while true; do
-        pidof `basename $DAEMON` >/dev/null
-        if [ $? -ne 0 ]; then
-            return 0
-        fi
-
-        sleep 1
-        n=$(expr $n + 1)
-        if [ $n -eq 30 ]; then
-            /bin/kill -s SIGKILL $PID
-            return 0
-        fi
-    done
+    kill -s $3 $pid
 }
 
 function log_failure_msg() {
@@ -122,28 +95,37 @@ function log_success_msg() {
     echo "$@" "[ OK ]"
 }
 
+# Process name ( For display )
+name=gde
+
+# Daemon name, where is the actual executable
+daemon=/usr/bin/gde
+
+# pid file for the daemon
+pidfile=/var/run/gde/gde.pid
+piddir=`dirname $pidfile`
+
+if [ ! -d "$piddir" ]; then
+    mkdir -p $piddir
+    chown $USER:$GROUP $piddir
+fi
+
+# Configuration file
+config=/etc/gde/gde.conf
+confdir=/etc/gde/gde.d
+
+# If the daemon is not there, then exit.
+[ -x $daemon ] || exit 5
+
 case $1 in
     start)
-        # Check if config file exist
-        if [ ! -r $CONFIG ]; then
-            log_failure_msg "config file doesn't exists"
-            exit 4
-        fi
-
         # Checked the PID file exists and check the actual status of process
-        if [ -e $PIDFILE ]; then
-            pidofproc -p $PIDFILE $DAEMON > /dev/null 2>&1 && STATUS="0" || STATUS="$?"
+        if [ -e $pidfile ]; then
+            pidofproc -p $pidfile $daemon > /dev/null 2>&1 && status="0" || status="$?"
             # If the status is SUCCESS then don't need to start again.
-            if [ "x$STATUS" = "x0" ]; then
-                log_failure_msg "$NAME process is running"
+            if [ "x$status" = "x0" ]; then
+                log_failure_msg "$name process is running"
                 exit 0 # Exit
-            fi
-        # if PID file does not exist, check if writable
-        else
-            su -s /bin/sh -c "touch $PIDFILE" $USER > /dev/null 2>&1
-            if [ $? -ne 0 ]; then
-                log_failure_msg "$PIDFILE not writable, check permissions"
-                exit 5
             fi
         fi
 
@@ -152,31 +134,54 @@ case $1 in
         ulimit -n $OPEN_FILE_LIMIT
         if [ $? -ne 0 ]; then
             log_failure_msg "set open file limit to $OPEN_FILE_LIMIT"
-            exit 1
         fi
 
-        log_success_msg "Starting the process" "$NAME"
-        if which start-stop-daemon > /dev/null 2>&1; then
-            start-stop-daemon --chuid $GROUP:$USER --start --quiet --pidfile $PIDFILE --exec $DAEMON -- -pidfile $PIDFILE -config $CONFIG $GDE_OPTS >>$STDOUT 2>>$STDERR &
+        log_success_msg "Starting the process" "$name"
+        if command -v startproc >/dev/null; then
+            startproc -u "$USER" -g "$GROUP" -p "$pidfile" -q -- "$daemon" -pidfile "$pidfile" -config "$config" -config-directory "$confdir" $GDE_OPTS
+        elif which start-stop-daemon > /dev/null 2>&1; then
+            start-stop-daemon --chuid $USER:$GROUP --start --quiet --pidfile $pidfile --exec $daemon -- -pidfile $pidfile -config $config -config-directory $confdir $GDE_OPTS >>$STDOUT 2>>$STDERR &
         else
-            su -s /bin/sh -c "nohup $DAEMON -pidfile $PIDFILE -config $CONFIG $GDE_OPTS >>$STDOUT 2>>$STDERR &" $USER
+            su -s /bin/sh -c "nohup $daemon -pidfile $pidfile -config $config -config-directory $confdir $GDE_OPTS >>$STDOUT 2>>$STDERR &" $USER
         fi
-        log_success_msg "$NAME process was started"
+        log_success_msg "$name process was started"
         ;;
 
     stop)
         # Stop the daemon.
-        if [ -e $PIDFILE ]; then
-            pidofproc -p $PIDFILE $DAEMON > /dev/null 2>&1 && STATUS="0" || STATUS="$?"
-            if [ "$STATUS" = 0 ]; then
-                if killproc -p $PIDFILE SIGTERM && /bin/rm -rf $PIDFILE; then
-                    log_success_msg "$NAME process was stopped"
+        if [ -e $pidfile ]; then
+            pidofproc -p $pidfile $daemon > /dev/null 2>&1 && status="0" || status="$?"
+            if [ "$status" = 0 ]; then
+                # periodically signal until process exists
+                while true; do
+                        if ! pidofproc -p $pidfile $daemon > /dev/null; then
+                                break
+                        fi
+                        killproc -p $pidfile SIGTERM 2>&1 >/dev/null
+                        sleep 2
+                done
+
+                log_success_msg "$name process was stopped"
+                rm -f $pidfile
+            fi
+        else
+            log_failure_msg "$name process is not running"
+        fi
+        ;;
+
+    reload)
+        # Reload the daemon.
+        if [ -e $pidfile ]; then
+            pidofproc -p $pidfile $daemon > /dev/null 2>&1 && status="0" || status="$?"
+            if [ "$status" = 0 ]; then
+                if killproc -p $pidfile SIGHUP; then
+                    log_success_msg "$name process was reloaded"
                 else
-                    log_failure_msg "$NAME failed to stop service"
+                    log_failure_msg "$name failed to reload service"
                 fi
             fi
         else
-            log_failure_msg "$NAME process is not running"
+            log_failure_msg "$name process is not running"
         fi
         ;;
 
@@ -187,22 +192,22 @@ case $1 in
 
     status)
         # Check the status of the process.
-        if [ -e $PIDFILE ]; then
-            if pidofproc -p $PIDFILE $DAEMON > /dev/null; then
-                log_success_msg "$NAME Process is running"
+        if [ -e $pidfile ]; then
+            if pidofproc -p $pidfile $daemon > /dev/null; then
+                log_success_msg "$name Process is running"
                 exit 0
             else
-                log_failure_msg "$NAME Process is not running"
+                log_failure_msg "$name Process is not running"
                 exit 1
             fi
         else
-            log_failure_msg "$NAME Process is not running"
+            log_failure_msg "$name Process is not running"
             exit 3
         fi
         ;;
 
     version)
-        $DAEMON version
+        $daemon version
         ;;
 
     *)
